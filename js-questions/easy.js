e1d8df8f58b05f69947554678feed70b59e6afc8
Shubham Laad <shubhamlaad666@gmail.com>
2023-05-03 13:29:09 +0530

    Add more questions

--- js-questions/easy.js
+++ js-questions/easy.js
@@ -1,71 +1,60 @@
-
-1 + '12'
-1 - '12'
-'4s' - 2
-'2' * '3'
-
-// write logic
-
-// abcaec
-// abcdef
-
-// shubhamlaad
-// shubcam
-
+1 + '12';
+1 - '12';
+'4s' - 2;
+'2' * '3';
 
 // Sort the array
-arr = [{ age: 25, name: 'Shubham' }, { age: 23, name: 'Ujjwal' }]
+arr = [
+  { age: 25, name: 'Shubham' },
+  { age: 23, name: 'Ujjwal' },
+];
 
+arr.sort((a, b) => a.age - b.age);
 
 //////////////////
 obj1 = {
-    a: 1,
-    b: {
-        c: 30
-    }
-}
+  a: 1,
+  b: {
+    c: 30,
+  },
+};
 
-obj2 = { ...obj1 }
-obj2.a = 20
-obj2.b.c = 40
+obj2 = { ...obj1 };
+obj2.a = 20;
+obj2.b.c = 40;
 
 // obj1.a
 // obj1.b.c
 
-
 //////
 
-obj2 = createDeepCopy(obj1)
+obj2 = createDeepCopy(obj1);
 
 ////////////////////////
-console.log(1)
+console.log(1);
 setTimeout(() => {
-    console.log(2)
-}, 0)
-console.log(3)
+  console.log(2);
+}, 0);
+console.log(3);
 
 ///////////////////////
 function checkOutput() {
-
-    var a = 1;
-    {
-        var b = 2;
-        console.log(a, b)
-
-    }
-    console.log(a, b)
+  var a = 1;
+  {
+    var b = 2;
+    console.log(a, b);
+  }
+  console.log(a, b);
 }
-checkOutput()
+checkOutput();
 
-console.log(a, b)
+console.log(a, b);
 
 //////////////////////
 function foo() {
-    return Promise.resolve(1)
+  return Promise.resolve(1);
 }
 
-console.log(1)
-foo().then(a => console.log(2))
-console.log(3)
-
-
+console.log(1);
+foo().then((a) => console.log(2));
+console.log(3);
--- js-questions/hard.js
+++ js-questions/hard.js
@@ -1,17 +1,33 @@
 // out put
 function y() {
-    console.log(this);
+  console.log(this);
 }
 var x = {
-    method: function () {
-        arguments[0]()
-    }
-}
-x.method(y, 1)
-
+  method: function () {
+    arguments[0]();
+  },
+};
+x.method(y, 1);
 
 //////////////////////////////
-a = [1, function () { return this }, 3]
-b = { 0: 1, 1: function () { return this } }
-b[1]()
-a[1]()
+a = [
+  1,
+  function () {
+    return this;
+  },
+  3,
+];
+b = {
+  0: 1,
+  1: function () {
+    return this;
+  },
+};
+b[1]();
+a[1]();
+
+////////////
+// write logic
+
+// abcaec
+// abcdef
